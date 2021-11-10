5c3a2b264e267fca6b64b19e1ebba08e6d1528f3
Khama Joshua <kowhegbajosh@gmail.com>
2021-11-10 14:56:58 +0100

    updated content

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app" class="flex h-screen w-screen overflow-hidden p-4">
+    <!-- 
+      Width of 16 by default, 32 on medium screens, and 48 on large screens 
+      <img class="w-16 md:w-32 lg:w-48" src="...">
+   -->
+
     <!-- side nav -->
     <div class="lg:w-1/4 px-2 lg:px-6 py-2 flex flex-col justify-between">
       <!-- Profile Account -->
@@ -70,49 +75,108 @@
       </div>
       <!-- End of sidebar -->
     </div>
+
     <!-- DASHBOARD -->
 
-    <div class="w-screen grid grid-rows-5 grid-flow-row gap-4 px-6">
-      <div class="row-span-2 bg-gray-200">
-        <div class="flex justify-end space-x-16">
+    <div
+      class="
+        w-screen
+        lg:grid lg:grid-rows-5 lg:grid-flow-row lg:gap-4
+        px-6
+        md:overflow-y-auto
+        sm:overflow-y-auto
+        md:flex md:flex-col
+        sm:flex sm:flex-col
+      "
+    >
+      <div class="lg:row-span-2 bg-gray-200 rounded-tr-3xl rounded-bl-3xl">
+        <div class="flex justify-end lg:space-x-16 lg:mx-16 my-8 lg:text-xl">
           <div>
-            <span>
-              <i class="far fa-sun"></i>
+            <span class="cursor-pointer">
+              <i class="far fa-sun mr-4"></i>
               <span>US Stock market opens in 3 hours</span>
             </span>
           </div>
           <div>
-            <span>
+            <span class="cursor-pointer">
               <span>Main Wallet</span>
-              <i class="fas fa-sort-down"></i>
+              <i class="fas fa-sort-down pl-2"></i>
             </span>
           </div>
-          <div>
-            <i class="far fa-bell"></i>
+          <div class="cursor-pointer">
+            <i class="far fa-bell w-8 h-8"></i>
           </div>
-          <div>
-            <i class="fas fa-plus"></i>
+          <div
+            class="
+              p-2
+              bg-black
+              rounded-full
+              h-8
+              w-8
+              flex
+              items-center
+              justify-center
+              cursor-pointer
+            "
+          >
+            <i class="fas fa-plus text-white text-sm"></i>
           </div>
         </div>
 
-        <div class="grid grid-cols-8 gap-16 h-full m-16">
-          <div class="col-span-3 rounded-3xl bg-white">1</div>
+        <div class="grid grid-cols-8 gap-16 h-4/5 mx-16">
+          <div class="col-span-3 rounded-3xl bg-white text-center">
+            <div class="mt-12 mb-2 text-3xl">
+              <span>Upgrade your Account</span>
+            </div>
+            <div class="flex">
+              <div class="flex-1 m-2">
+                <img
+                  src="img1.png"
+                  class="w-52 h-full object-left-bottom -ml-16"
+                />
+              </div>
+              <div class="flex-1 text-left mr-10 space-y-4">
+                <div class="row-span-2 text-xl block">
+                  <span class="text-gray-500"
+                    >Access many other features and helpful insights.</span
+                  >
+                </div>
+                <div
+                  class="
+                    p-2
+                    bg-black
+                    rounded-lg
+                    h-16
+                    w-16
+                    items-center
+                    justify-center
+                    cursor-pointer
+                    block
+                    flex
+                    float-right
+                  "
+                >
+                  <i class="fas fa-arrow-right text-white text-3xl"></i>
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="col-span-5 rounded-3xl bg-white">2</div>
         </div>
       </div>
 
       <!-- Portfolio History -->
-      <div class="row-span-2">
-        <div class="flex justify-between space-x-16">
+      <div class="lg:row-span-2">
+        <div class="flex justify-between space-x-16 mx-16 mt-8 mb-2 lg:text-xl">
           <div class="">
             <span>Portfolio History</span>
           </div>
-          <div class="">
+          <div class="underline">
             <span>Per Industry</span>
           </div>
         </div>
 
-        <div class="grid grid-cols-9 gap-8 h-full m-16">
+        <div class="grid grid-cols-9 gap-8 h-4/5 mx-16">
           <div class="col-span-2 border-2 rounded-3xl"></div>
           <div class="col-span-2 border-2 rounded-3xl"></div>
           <div class="col-span-2 border-2 rounded-3xl"></div>
@@ -122,7 +186,7 @@
 
       <!-- Money Table -->
       <div class="row-span-1 justify-center flex items-end">
-        <table class="table-auto w-full text-right px-6 mx-12 flex-shrink">
+        <table class="table-auto w-11/12 text-right px-6 border-l m-16">
           <thead>
             <tr>
               <th>SYMBOL</th>
@@ -180,3 +244,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#app {
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+</style>
